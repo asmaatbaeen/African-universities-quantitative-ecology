5ceea868cb8bda9cf2f14c7b90784ba9969b2c26
Asmaa Tbaeen <asmaatbaeen@Asmaas-MacBook-Pro.local>
2023-07-13 09:36:17 +0200

    add

--- scripts.py
+++ scripts.py
@@ -1,10 +1,25 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-def map_plot(africa_shapefile,program_df,fig_size=(10, 10)):
+ 
+
+def map_plot(africa_shapefile, program_df, title, fig_size=(10, 10)):
+    """
+    Plot the locations of quantitative statistical ecology programs in Africa on a map and save the plot.
+
+        Inputs:
+        - africa_shapefile (str): The file path to the shapefile or GeoJSON file containing African country boundaries.
+        - program_df (pandas DataFrame): The DataFrame containing the program data, including latitude and longitude columns.
+        - title (str): The title for the plot and filename when saving.
+        - fig_size (tuple, optional): The size of the figure (plot) in inches. Default is (10, 10).
+
+        Outputs:
+        - None
+
+    """
 
     # Load shapefile or GeoJSON file of African country boundaries
-    africa_shapefile = africa_shapefile # Replace with the actual file path
+    africa_shapefile = africa_shapefile  # Replace with the actual file path
 
     longitudes = program_df["longitude"]
     latitudes = program_df["latitude"]
@@ -28,10 +43,13 @@ def map_plot(africa_shapefile,program_df,fig_size=(10, 10)):
     program_gdf.plot(ax=ax, color="red", markersize=50)
 
     # Set plot title and labels
-    ax.set_title("Quantitative Statistical Ecology Programs in Africa")
+    ax.set_title(title)
     ax.set_xlabel("Longitude")
     ax.set_ylabel("Latitude")
 
+    # Save the plot with the provided title as the filename
+    filename = f"{title}.png"
+    plt.savefig(filename)
+
     # Show the plot
     plt.show()
- 
